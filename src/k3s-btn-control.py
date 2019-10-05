b574f16ef5a42f52e7929cdbb31dc82ddd900e4b
Mark Abrams <mabrams@helpguest.com>
2019-10-05 17:48:00 -0400

    added some addition functions to buttons B and C. first pass at using subprocess vice os

--- src/k3s-btn-control.py
+++ src/k3s-btn-control.py
@@ -3,26 +3,70 @@
 import signal
 import buttonshim
 import os
+import subprocess
 
 print("""
 Control the k3s-arm-demo
 Press Ctrl+C to exit.
 """)
 
-deployDSPin4 = 'kubectl --kubeconfig=/home/pi/kubeconfig.yaml apply -f /home/pi/workloads/blue-ds.yaml'
-undeployDSPin4 = 'kubectl --kubeconfig=/home/pi/kubeconfig.yaml delete -f /home/pi/workloads/blue-ds.yaml'
 buttonAHold = False
+deployDSPin4 = ["kubectl", "--kubeconfig=/home/pi/kubeconfig.yaml", "apply", "-f /home/pi/workloads/blue-ds.yaml"]
+undeployDSPin4 = 'kubectl --kubeconfig=/home/pi/kubeconfig.yaml delete -f /home/pi/workloads/blue-ds.yaml'
 
 @buttonshim.on_release(buttonshim.BUTTON_A)
 def button_a(button, released):
+    global buttonAHold
     if buttonAHold == False:
-        os.system(deployDSPin4)
+        subprocess.check_call(deployDSPin4)
     buttonAHold = False
 
 @buttonshim.on_hold(buttonshim.BUTTON_A)
 def button_a(button):
+    global buttonAHold
     os.system(undeployDSPin4)
     buttonAHold = True
+
+
+buttonBHold = False
+deployPodPin5 = 'kubectl --kubeconfig=/home/pi/kubeconfig.yaml apply -f /home/pi/workloads/white-pod.yaml'
+undeployPodPin5 = 'kubectl --kubeconfig=/home/pi/kubeconfig.yaml delete -f /home/pi/workloads/white-pod.yaml'
+scaleUpPodPin5 = 'kubectl --kubeconfig=/home/pi/kubeconfig.yaml scale --replicas=3 pod/white-pod'
+
+@buttonshim.on_release(buttonshim.BUTTON_B)
+def button_b(button, released):
+    global buttonBHold
+    if buttonBHold == False:
+        os.system(deployPodPin5)
+    buttonBHold = False
+
+@buttonshim.on_hold(buttonshim.BUTTON_B)
+def button_b(button):
+    global buttonBHold
+    os.system(undeployPodPin5)
+    buttonBHold = True
+
+
+buttonCHold = False
+deployPodPin5 = 'kubectl --kubeconfig=/home/pi/kubeconfig.yaml apply -f /home/pi/workloads/white-pod.yaml'
+undeployPodPin5 = 'kubectl --kubeconfig=/home/pi/kubeconfig.yaml delete -f /home/pi/workloads/white-pod.yaml'
+scaleUpPodPin5 = 'kubectl --kubeconfig=/home/pi/kubeconfig.yaml scale --replicas=3 pod/white-pod'
+
+@buttonshim.on_release(buttonshim.BUTTON_C)
+def button_c(button, released):
+    global buttonCHold
+    if buttonCHold == False:
+        os.system(scaleUpPodPin5)
+    buttonCHold = False
+
+@buttonshim.on_hold(buttonshim.BUTTON_C)
+def button_c(button):
+    global buttonCHold
+    os.system(undeployPodPin5)
+    buttonCHold = True
+
+
+# On press default pimoroni actions
 
 @buttonshim.on_press(buttonshim.BUTTON_A)
 def button_a(button, pressed):
